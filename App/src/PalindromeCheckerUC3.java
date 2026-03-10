7e9efd14aecd9c9d95d8e72b47fb01523f778b5a
Nishant-codess <nishant.ranjan.air1@gmail.com>
2026-03-10 15:00:34 +0530

    Add UC3 Palindrome Checker App implementation

---
+++ App/src/PalindromeCheckerUC3.java
@@ -0,0 +1,17 @@
+public class PalindromeCheckerUC3 {
+
+    public static void main(String[] args) {
+        String original = "level";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("The string \"" + original + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + original + "\" is not a palindrome.");
+        }
+    }
+}
